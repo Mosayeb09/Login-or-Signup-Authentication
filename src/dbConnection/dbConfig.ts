f3365a3c71266df544e1e5f828e06239bed72b18
Mosayeb09 <mosayebhossain09@gmail.com>
2024-11-07 17:50:03 +0600

    connect and postman check

--- src/dbConnection/dbConfig.ts
+++ src/dbConnection/dbConfig.ts
@@ -1,18 +1,22 @@
 import mongoose  from "mongoose";
 
+
 export async function connect() {
 
     try{
 
-       mongoose.connect(process.env.MONGO_URI) 
+       mongoose.connect(process.env.MONGO_URI! )
 
-       const connection = mongoose.Connection
-
-       connection.on('conneted', ()=> {
+    //    const connection = mongoose.Connection
+       mongoose.connection.on('connected',()=>{
         console.log('MongoBD connected');
        })
 
-       connection.on('error',(err)=> {
+    //    connection.on('conneted', ()=> {
+    //     console.log('MongoBD connected');
+    //    })
+
+       mongoose.connection.on('error',(err)=> {
         console.log('MongoDB connection error , please make sure db is up and running' + err);
         process.exit()
        })
